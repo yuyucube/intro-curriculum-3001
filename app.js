4def9f088d4befdb5ea35c2e1407cb574378ea49
yuyucube <yuyu_cube@hotmail.com>
2020-04-18 11:22:59 +0900

    階乗関数の実装

--- app.js
+++ app.js
@@ -7,7 +7,9 @@
 */
 function factorial(n) {
     let result = 1;
-    // TODO このコメントを消して正しく実装してください。
+    for( let i=n; i>0; i-- ){
+        result = i * result;
+    }
     return result;
 }
 const assert = require('assert');
